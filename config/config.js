ae2c49e8bbe316ca14ffe41c4101d4edaef976bb
J Olson <jo7168@gmail.com>
2013-05-10 11:53:31 -0500

    Initial add

---
+++ config/config.js
@@ -0,0 +1,10 @@
+// Register a new application at http://dev.twitter.com
+// Tracklytics
+module.exports = {
+   mongodb: 			'mongodb://<user>:<password>@<address>:<port>/<db>'
+   destinationCollection: 	'<collection>'
+   boundingBox: 		'<latitude>,<longitude>'
+   radius: 			'<radius>'
+   pagesToReturn: 		'<pagesToReturn>
+   searchPhrase:		'<escaped search string>'  /*food%20poison*/
+}
