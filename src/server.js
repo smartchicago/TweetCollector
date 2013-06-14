0dcbb32c4f8334bd2986de5f1554834b553be226
J Olson <jo7168@gmail.com>
2013-06-14 11:26:35 -0500

    converted to Twitter API v1.1

--- src/server.js
+++ src/server.js
@@ -1,57 +1,70 @@
 var dateFormat = require("dateformat");
 var conf = require("../config/config.js");
+var OAuth = require('oauth');
+
 var collections = [conf.destinationCollection];
 var db = require("mongojs").connect(conf.mongodb, collections);
-var request = require('request');
 
 console.log(dateFormat(new Date(),"yyyymmdd h:MM:ss")+" Starting run...")
+
 /*Get the largest id in the current collection */
 db.collection(conf.destinationCollection).find({},{id_str:1}).sort({id:-1}).limit(1, function(err, maxTweet) {
 	if (err)
 	{
 		/*If err, something is wrong with Mongo, exit*/
- 		console.log(dateFormat(new Date(),"yyyymmdd h:MM:ss")+"    Database getMaxTweet error! "+err);
+  		console.log(dateFormat(new Date(),"yyyymmdd h:MM:ss")+" Database getMaxTweet error! "+err);
 	}
 	else {
-		/*Form up request*/
-		console.log(dateFormat(new Date(),"yyyymmdd h:MM:ss")+"    Forming request. Max Id: "+maxTweet[0].id_str)
-		url = 'http://search.twitter.com/search.json?q='+conf.searchPhrase+'&geocode='+conf.boundingBox+','+conf.radius+'km&rpp='+conf.pagesToReturn+'&since_id='+maxTweet[0].id_str;
-		/*Send request to twitter*/
-		request(url, function (error, response, body) {
-		  if (!error && response.statusCode == 200) {
-			var tweetResponse = JSON.parse(body);
-			console.log(dateFormat(new Date(),"yyyymmdd h:MM:ss")+"    Number of new tweets: "+tweetResponse.results.length)		
-			tweetResponse.results.forEach(function(tweet) {
-				/*Add it*/
-			        console.log(dateFormat(new Date(),"yyyymmdd h:MM:ss")+"    Adding tweet: "+tweet.id_str)
-				/*Add java date time to save time later*/
-			        tweet.jDate = new Date(tweet.created_at);
-				/*Write it to Mongo*/
-			        var encodedText =
-			        db.collection(conf.destinationCollection).insert(tweet,function(err) {
-			                if (err) {
-						/*Mongo bombs out on write*/
-			                        console.log(dateFormat(new Date(),"yyyymmdd h:MM:ss")+"    Database insert error: "+err);
-			                }
-			        });
-			});
-		  }
-		  else if (error) {
-		    /*Request to twitter fails*/
-		    console.log(dateFormat(new Date(),"yyyymmdd h:MM:ss")+"    Twitter request error: "+error);
-		  }
-		  else {
-		    /*Twitter responds with something other that a http 200*/
-		    console.log(dateFormat(new Date(),"yyyymmdd h:MM:ss")+"    Twitter bad response: "+ response.statusCode);
-		  }
-		});
+		console.log(dateFormat(new Date(),"yyyymmdd h:MM:ss")+" Forming request. Max Id: "+maxTweet[0].id_str)
+		var url = 'https://api.twitter.com/1.1/search/tweets.json?q='+conf.searchPhrase+'&geocode='+conf.boundingBox+','+conf.radius+'km&rpp='+conf.pagesToReturn+'&since_id='+maxTweet[0].id_str;
+
+		var oauth = new OAuth.OAuth(
+		      'https://api.twitter.com/oauth/request_token',
+		      'https://api.twitter.com/oauth/access_token',
+		      conf.twitter.consumerKey,
+		      conf.twitter.consumerSecret,
+		      '1.0A',
+		      null,
+		      'HMAC-SHA1'
+		    );
+
+		oauth.get(
+		      url,
+		      conf.twitter.accessTokenKey,
+		      conf.twitter.accessTokenSecret,
+		      function (e, data, res) {
+			   if (e) console.error(e);
+			   var tweetResponse = JSON.parse(data);
+			   tweetResponse.statuses.forEach(function(tweet) {
+			   db.collection(conf.destinationCollection).findOne({id_str:tweet.id_str},function(err,atweet){
+				if (err) {
+					console.log(dateFormat(new Date(),"yyyymmdd h:MM:ss")+" Insert error: "+err)
+				}
+				if (atweet) {
+				        console.log(dateFormat(new Date(),"yyyymmdd h:MM:ss")+" Existing Tweet: "+tweet.id_str) // Print the tweet.
+				}
+				else {
+				        console.log(dateFormat(new Date(),"yyyymmdd h:MM:ss")+" Adding Tweet: "+tweet.id_str) // Print the tweet.
+				        tweet.jDate = new Date(tweet.created_at);
+				        tweet.from_user = tweet.user.screen_name;
+				        //var encodedText =
+				        db.collection(conf.destinationCollection).insert(tweet,function(err) {
+				                if (err) {
+				                        console.log(dateFormat(new Date(),"yyyymmdd h:MM:ss")+" Error: "+err);
+				                } 
+				        });
+
+				}
+
+
+			     });
+			   });
+		       }
+		);
 	}
 });
-/*Fix this. How do we know all the work is done?*/
+
 setTimeout(function(){
   process.exit(0);
 }, 10000);
-
-
-
 
